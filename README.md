b0c85190b8b636fd663a4062c84f8dbbad62a96d
Brice Amen <brice@briceamen.com>
2022-09-14 18:16:31 +0200

    First iteration of the script + README.md

---
+++ README.md
@@ -0,0 +1,64 @@
+# SteamCMD_Watcher
+
+SteamCMD_Watcher is a simple bash monitoring script for Linux allowing you automatically start your server if stopped or watch if an update is available and then restart the server.
+
+## How to setup
+
+Open the script and edit those lines with your own configuration
+
+(Here is an example for Conan Exiles)
+
+```bash
+# /!\ CHANGE THIS /!\
+USER_PATH="/home/steam"
+LOG_PATH="/home/steam/server/Logs"
+RCON = "true"
+RCON_PASSWORD="PASSWORD" # If your game server use RCON enter the password here (you'll need to install mcrcon)
+SERVER_EXE_NAME="ConanSandboxServer-Win64-Test.exe"
+APP_ID="443030"
+# Find your game server ID here : https://developer.valvesoftware.com/wiki/SteamCMD#Game_Servers
+
+# /!\ CHANGE THIS /!\ Create a variable for the path of steamcmd where the steamcmd.sh file is located 
+STEAMCMD=$USER_PATH/.steam/steam/steamcmd/steamcmd.sh
+
+# /!\ CHANGE THIS /!\ Create a variable where the .exe server file is located
+SERVER=$USER_PATH/server/conan/ConanSandbox/Binaries/Win64/ConanSandboxServer-Win64-Test.exe
+```
+
+Make the script executable
+
+```bash
+chmod +x SteamCMD_Watcher.sh
+```
+
+Run the script
+
+```bash
+./SteamCMD_Watcher.sh 
+```
+
+Additionnaly, you can run the script and only call one function
+
+```bash
+./SeamCMD_Watcher.sh check_server
+```
+
+or
+
+```bash
+./SeamCMD_Watcher.sh update_server
+```
+
+### Some cron jobs
+
+Cron job to check if the server is running every 5 minutes and log the output silently
+
+```bash
+5 * * * * /home/steam/SteamCMD_Watcher.sh check_server >> /home/steam/server/Logs/SteamCMD_Watcher.log 2>&1
+```
+
+Cron job to check if an update is available every 30 minutes and log the output silently
+
+```bash
+30 * * * * /home/steam/SteamCMD_Watcher.sh update_server >> /home/steam/server/SteamCMD_Watcher.log 2>&1
+```
---
+++ SteamCMD_Watcher.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+#Script Name : SteamCMD_Watcher.sh
+#Description : Script for monitoring and updating game servers for Linux SteamCMD
+#Author : bnema
+
+# /!\ CHANGE THIS /!\
+USER_PATH="/home/steam"
+LOG_PATH="/home/steam/server/Logs"
+RCON = "true"
+RCON_PASSWORD="PASSWORD"
+SERVER_EXE_NAME="ConanSandboxServer-Win64-Test.exe"
+APP_ID="443030"
+# Find your game server ID here : https://developer.valvesoftware.com/wiki/SteamCMD#Game_Servers
+
+# /!\ CHANGE THIS /!\ Create a variable for the path of steamcmd where the steamcmd.sh file is located 
+STEAMCMD=$USER_PATH/.steam/steam/steamcmd/steamcmd.sh
+
+# /!\ CHANGE THIS /!\ Create a variable where the .exe server file is located
+SERVER=$USER_PATH/server/conan/ConanSandbox/Binaries/Win64/ConanSandboxServer-Win64-Test.exe
+
+
+# ((((((((((((((((((((((((((((((((((((()))))))))))))))))))))))))))))))))))
+# ((((((((((((((((((((((((CHANGE AT YOUR OWN RISK)))))))))))))))))))))))))
+# ((((((((((((((((((((((((((((((((((((()))))))))))))))))))))))))))))))))))
+
+# Get the pid of the server with awk
+PID=$(ps -ef | grep $SERVER_EXE_NAME | grep -v 'grep' | grep -v '/bin/sh' | awk '{ printf $2 }')
+
+
+# Function to check if the server is running every x minutes for crontab and write the result in a log file
+
+function check_server() {
+    # Check if the server is running with grep and write the result in a log file
+    if ps -p $PID > /dev/null
+    then
+        echo "Server $PID is running" >> $LOG_PATH/SteamCMD_Watcher.log | ts '[%Y-%m-%d %H:%M:%S]'
+    else
+        echo "Server is not running starting the server..." >> $LOG_PATH/SteamCMD_Watcher.log | ts '[%Y-%m-%d %H:%M:%S]'
+        # If the server is not running, start the server
+        xvfb-run --auto-servernum wine64 $SERVER/ConanSandboxServer-Win64-Test.exe -log -server 
+    fi
+}
+
+# Function to check if an update is available and download it with SteamCMD
+# If an update is available, the server will be restarted
+
+# Ask steamcmd to check for updates
+function update_server() {
+$STEAMCMD +force_install_dir /home/steam/server +login anonymous +app_update $APP_ID +app_update +quit | tee $LOG_PATH/steam_update.log
+    # Search in the log file if the update was successful
+    if grep -q "Success! App '443030' fully installed." $LOG_PATH/steam_update.log; 
+    then
+        echo "Update successful, restarting server..."
+        if RCON = "true"
+        then  
+        mcrcon -p $RCON_PASSWORD "broadcast Une mise à jour est disponible pour Conan redémarrage automatique dans 15 minutes, mettez- vous à l'abris !"
+        sleep 5m
+        mcrcon -p $RCON_PASSWORD "broadcast Une mise à jour est disponible pour Conan redémarrage automatique dans 10 minutes."
+        sleep 5m
+        mcrcon -p $RCON_PASSWORD "broadcast Une mise à jour est disponible pour Conan redémarrage automatique dans 5 minutes,"
+        sleep 4m
+        mcrcon -p $RCON_PASSWORD "broadcast Une mise à jour est disponible pour Conan redémarrage automatique dans 1 minute."
+        else
+        echo "RCON is not activated, restarting the server right now..."
+        fi
+        # Stop the server and wait for the process to end
+        SIGINT $PID
+        trap SIGINT $PID
+        # Wait for the server to close
+        wait $PID
+        sleep $WATCHDOG_TIME
+        # Start the server
+        xvfb-run --auto-servernum wine64 $SERVER/ConanSandboxServer-Win64-Test.exe -log -server 
+        # clear the log file
+        > $LOG_PATH/steam_update.log
+    else
+        echo "No update available"
+    fi
+}
+#Call functions
+check_server
+update_server
+
+
+# Cron job to check if the server is running every 5 minutes and log the output silently
+# 5 * * * * /home/steam/SteamCMD_Watcher.sh check_server >> /CHANGE_THE_DIRECTORY_HERE/Logs/SteamCMD_Watcher.log 2>&1
+
+# Cron job to check if an update is available every 30 minutes and log the output silently
+# 30 * * * * /home/steam/SteamCMD_Watcher.sh update_server >> /CHANGE_THE_DIRECTORY_HERE/SteamCMD_Watcher.log 2>&1
+exit 0
